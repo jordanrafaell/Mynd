e053040ddf816d6f57618b70a4d556401b01f04c
Jordan Rafael <61181764+jordanrafaell@users.noreply.github.com>
2025-05-29 06:51:59 -0300

    criação de sessão

---
+++ src/config/session.ts
@@ -0,0 +1,4 @@
+// src\config\session.ts
+export const SESSION_CLIENT = 'session-client';
+export const SESSION_SUPORT = 'session-suport';
+
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { create } from 'venom-bot';
+import { startMessage } from './client/index';
+import { SESSION_CLIENT } from './config/session';
+
+create({
+  session: SESSION_CLIENT,
+  headless: 'new',
+  disableWelcome: true,
+  logQR: true
+})  .then((client) => {
+    console.log('🟢 Bot iniciado com sucesso');
+    startMessage(client);
+  })
+  .catch((error) => {
+    console.log('🔴 Erro ao iniciar o bot: ', error);
+  });
